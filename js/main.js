1d6e8a36f9bb83d136465854f60a042baea9d506
Szymon1111 <40290583+Szymon1111@users.noreply.github.com>
2021-03-11 22:02:31 +0100

    Ekran ładowania

--- js/main.js
+++ js/main.js
@@ -5,3 +5,9 @@ navOpener.addEventListener("click", () => {
   pageNav.classList.toggle("opened");
   navOpener.classList.toggle("opened");
 });
+
+window.addEventListener("load", () => {
+  window.setTimeout(() => {
+    document.querySelector(".loading").style.display = "none";
+  }, 2000);
+});
